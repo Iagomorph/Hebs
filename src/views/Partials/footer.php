cce8021dadd837924d4364025d5b6d09b4828c99
Jules <jules.duarte@edu.esiee-it.fr>
2022-02-11 14:59:08 +0100

    changement footer

--- src/views/Partials/footer.php
+++ src/views/Partials/footer.php
@@ -1,29 +1,31 @@
 
 <footer class="footer-distributed">
 
-			<div class="footer-right">
-
-				<a href="#"><i class="fa fa-facebook"></i></a>
-				<a href="#"><i class="fa fa-twitter"></i></a>
-				<a href="#"><i class="fa fa-linkedin"></i></a>
-				<a href="#"><i class="fa fa-github"></i></a>
-
-			</div>
-
 			<div class="footer-left">
 
 				<p class="footer-links">
-					<a class="link-1" href="#">Home</a>
+				<a href="index.php?p=article">Marketplace</a>
+				<?php
+					if(!empty($_SESSION['id'])){
+					?>
+					<a href="index.php?p=profil">Mon profil</a>
+					<a href="index.php?p=panier">Mon panier</a>
+					<?php if($_SESSION['admin'] == 1){
+					?>
+					<a href="index.php?p=admin">Modération</a>
+					<?php
+					};
+					?>
+					<a href="actions/actionLogout.php">Déconnexion</a>
 
-					<a href="#">Blog</a>
-
-					<a href="#">Pricing</a>
-
-					<a href="#">About</a>
-
-					<a href="#">Faq</a>
-
-					<a href="#">Contact</a>
+					<?php
+					}else{
+					?>
+					<a href="index.php?p=signup">S'inscrire</a>
+					<a href="index.php?p=login">Connexion</a>
+					<?php
+					};
+					?>
 				</p>
 
 				<p>KrukmakProd © All Right Reserved ltd</p>
